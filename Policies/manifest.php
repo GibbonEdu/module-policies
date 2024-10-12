003af436f528a851b3e0b0213335ca44215f2158
Ross Parker <>
2024-10-12 20:40:05 +0800

    v2.2.01

--- Policies/manifest.php
+++ Policies/manifest.php
@@ -27,7 +27,7 @@ $description = 'A place to store, share and view school policies, either as file
 $entryURL = 'policies_view.php';
 $type = 'Additional';
 $category = 'Other';
-$version = '2.2.00';
+$version = '2.2.01';
 $author = "Gibbon Foundation";
 $url = "https://gibbonedu.org";
 
